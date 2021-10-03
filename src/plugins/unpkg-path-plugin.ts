c8936a5657063a7bd1a56275e10fb8b10369f493
Szymon Kucharski <speckope@gmail.com>
2021-10-03 22:10:47 +0200

    caching packages in Indexed DB!

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,29 +1,25 @@
 import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name: 'filecache',
+});
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      // onResolve step figures out, where the index.js file is stored
-      // By defining it, we change the default behavior of esbuild and change
-      // We return the path ourselves
-      // WE can control when onResolve and onLoad are executed with filter: ...
       build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => {
         console.log('onResolve', args);
-        // When we return namespace, we can say in a next function that we want a function to pplay ony to
-        // the specified namespace. E.g. byy defining second argument in onLoad namespace: 'a'
 
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
         }
 
-        // If there is an import inside our module
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            // Importer is the file in which a module is, file which requires other module.
-            // .resolveDir is what we passed in a last onLoad object!
             path: new URL(
               args.path,
               'https://unpkg.com' + args.resolveDir + '/'
@@ -37,15 +33,9 @@ export const unpkgPathPlugin = () => {
         };
       });
 
-      // onLoad takes the path from onResolve and attempts to load this file up
-      // By defining it here, we change the default bahaviour of esbuild
-      // (which is to read it directly from the filesystem)
       build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
         console.log('onLoad', args);
 
-        // We return the object imnmediatlely it is trying to load!
-        // If esbuild is trying to load up index.js, don't let it load from the filesystem
-        // Instead we load it for esbuild!
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
@@ -55,17 +45,30 @@ export const unpkgPathPlugin = () => {
               console.log(message);
             `,
           };
-          // This runs when esbuild is trying to load up contents of a file that's not index.js
-          // So when there is an import statement!
-          // Don't try to access the filesystem, but load what we return.
         }
+
+        // Chack to see if we have already fetched this file
+        // and if it's already in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        // If it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
-          // This gets us the directory of one above file(folder)
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        // Store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
