0e06bd46ae7674af9532b2315f5fb4f8b75fa16d
Szymon Kucharski <speckope@gmail.com>
2021-10-15 17:42:31 +0200

    Implemented opting out of shared environemnt!

--- src/components/hooks/useCumulativeCode.ts
+++ src/components/hooks/useCumulativeCode.ts
@@ -31,25 +31,35 @@ export const useCumulativeCode = (cellId: string) => {
     // noop - no operation. Show that doesn't do anything
     const showFuncNoop = 'var show = () => {};';
 
+    // If we run this function for cell without shared environemnt
+    if (!data[cellId].sharedEnvironment) {
+      return [showFunc, data[cellId].content];
+    }
+
     // Loop over cells and make an array with code in order, when cell is of code type
     const cumulativeCode: string[] = [];
     for (let id of order) {
       // Push content to new array
       if (data[id].type === 'code') {
         // Insert before code a show function
-        if ((id = cellId)) {
+        if (id === cellId) {
           cumulativeCode.push(showFunc);
         } else {
           // If it's a code of a cell before, insert noop show.
           // This is so show in previous cells doesn't affect show in current cell.
           cumulativeCode.push(showFuncNoop);
         }
-        cumulativeCode.push(data[id].content);
+
+        // Push code into cumulative only if it shares environment
+        if (data[id].sharedEnvironment) {
+          cumulativeCode.push(data[id].content);
+        }
       }
       // Break early if we go to id of current cell
       if (id === cellId) break;
     }
     // Return code array
+
     return cumulativeCode;
   }).join('\n');
 };
--- src/state/store.ts
+++ src/state/store.ts
@@ -2,8 +2,13 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { ActionType } from './action-types';
 import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(
+  reducers,
+  {},
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 store.dispatch({
   type: ActionType.INSERT_CELL_AFTER,
