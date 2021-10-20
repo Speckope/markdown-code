ba9802659fed5d0cb8772f5e80f04fc4891b6b39
Szymon Kucharski <speckope@gmail.com>
2021-10-20 16:19:00 +0200

    folder name change

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log(123);
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hi There! Good Luck getting it all together! Work well ;)');
+});
+
+app.listen(4000, () => {
+  console.log('Listening on 4000...');
+});
